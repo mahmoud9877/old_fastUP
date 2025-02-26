369215ad5e4137ae49e1826bbd9454e7c4c4b8bf
mahmoud9877 <mohmoudali9877@gmail.com>
2025-02-26 12:46:09 +0200

    another update

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use("/api/v1", shipmentRouter);
 // app.use("/api/v1", shipmentRouter);
 
 // testing api
-app.get("/test", (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "API is working",
